92787d13ff1b6ac855e0b77ae92ff740cd283e6c
Mickey Gousset <mickeygousset@github.com>
2025-01-30 16:01:37 -0600

    Add workflow to test Mickey's Hello World action and update output variable

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const github = require('@actions/github');
 
 try {
   // Get the input value
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput('name');
   const message = `Hello, ${nameToGreet}!`;
   console.log(message);
 
   // Set the output variable
-  core.setOutput("greeting", message);
-
-  // Optionally, you can set more outputs
-  core.setOutput("time", new Date().toTimeString());
+  core.setOutput("phrase", message);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
